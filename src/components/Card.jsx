026e6b3a25910cb3172d018143c08105401f91b7
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-05-13 19:03:26 -0300

    Criando e renderizando o componente Card com as props necessárias

---
+++ src/components/Card.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Card extends Component {
+  render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+    return (
+      <div>
+        <h1 data-testid="name-card">{ cardName }</h1>
+        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
+        <h2 data-testid="description-card">{ cardDescription }</h2>
+        <h2 data-testid="attr1-card">{ cardAttr1 }</h2>
+        <h2 data-testid="attr2-card">{ cardAttr2 }</h2>
+        <h2 data-testid="attr3-card">{ cardAttr3 }</h2>
+        <h2 data-testid="rare-card">{ cardRare }</h2>
+        <h2>
+          {cardTrunfo !== false ? (
+            <h2 data-testid="trunfo-card">Super Trunfo</h2>
+          ) : (
+            <> </>
+          )}
+        </h2>
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+};
